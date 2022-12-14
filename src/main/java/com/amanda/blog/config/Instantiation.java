a07c8062eee77932244edea9234dd70a8cea79a2
Amanda Campos <amandaolivcampos@gmail.com>
2022-12-14 15:19:24 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/amanda/blog/config/Instantiation.java
@@ -0,0 +1,25 @@
+package com.amanda.blog.config;
+
+import com.amanda.blog.domain.User;
+import com.amanda.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
