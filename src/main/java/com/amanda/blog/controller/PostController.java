b64b3df8d02997eeab46d133447ff191bd79d22b
Amanda Campos <amandaolivcampos@gmail.com>
2022-12-18 17:25:07 -0300

    Consulta com vários critérios

--- src/main/java/com/amanda/blog/controller/PostController.java
+++ src/main/java/com/amanda/blog/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -29,4 +30,19 @@ public class PostController {
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body((list));
     }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body((list));
+    }
+
 }
